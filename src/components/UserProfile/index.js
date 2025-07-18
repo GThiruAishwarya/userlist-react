651110256d2d21f17c458a1d656427fb730bb287
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-18 11:43:51 +0530

    Update index.js

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -1,9 +1,10 @@
+import './index.css'
 const UserProfile=(props)=>{
   const {userDetails}=props
   const {imageUrl}=userDetails
 return(
-<div>
-  <img src={imageUrl}/>
+<div className="user-card-container">
+  <img src={imageUrl} className="avatar"/>
   </div>
 )
 }
