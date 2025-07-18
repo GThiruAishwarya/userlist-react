b632136185cbd6d08b11b30a2732db56336c7094
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-18 11:35:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,5 +4,5 @@ imageUrl:"https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
 name:"Rahul",
 role:"Software Developer"
 }
-const App=()=><UserProfile />
+const App=()=><UserProfile userDetails={userDetails}/>
 export default App;
