8bee11698ef9a045835e58649b4467e1398083b3
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-07-18 11:32:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import UserProfile from './components/UserProfile/index.js';
 const userDetails={
 imageUrl:"https://assets.ccbp.in/frontend/react-js/esther-howard-img.png",
 name:"Rahul",
 role:"Software Developer"
 }
-const App=()=><h1>User's List</h1>;
+const App=()=><UserProfile />
 export default App;
